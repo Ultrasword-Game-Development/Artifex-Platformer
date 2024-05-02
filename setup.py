966874866b9bcd511297534e3c0d486739c77584
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2024-05-02 12:14:58 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+# standard setup.py file
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Artifex-Platformer',
+    version='0.1',
+    description="A platformer game called 'Artifex' where the player must navigate through a series of trecherous levels, collect and combine spirits, to save the love of their life.",
+    license='MIT',
+    author='Peter Zhang',
+    install_requires=[
+        "pygame-ce==2.4.1",
+        "moderngl==5.10.0"
+    ]
+) 
